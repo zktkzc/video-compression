f677dd74ec78c4b3243f5c4173338cfeab64c3a6
tkzc00 <2238983030@qq.com>
2025-08-16 17:38:43 +0800

    初始化项目

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="bg-red-600">abc</main>
+</template>
---
+++ src/renderer/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@renderer/assets/tailwind.css'
+
+createApp(App).mount('#app')
