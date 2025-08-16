c90344b651372eb6cce4c11b64f34f84d79ddc15
tkzc00 <2238983030@qq.com>
2025-08-17 00:13:32 +0800

    完成进度条样式设计

---
+++ src/renderer/src/components/VideoItem.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { CloseOne } from '@icon-park/vue-next'
+import { ref } from 'vue'
+
+interface VideoType {
+  title: string
+}
+
+defineProps<{ video: VideoType }>()
+
+const process = ref(60)
+</script>
+
+<template>
+  <section class="video" :style="`--process: ${process}%;`">
+    <div class="title">{{ video.title }}</div>
+    <div class="icon">
+      <CloseOne theme="outline" size="12" />
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.video {
+  @apply bg-white px-3 py-[8px] rounded-lg mb-2 mx-3 text-xs text-slate-600 
+  flex justify-between items-center relative;
+
+  &::before {
+    content: '';
+    @apply bg-yellow-300 absolute top-0 bottom-0 left-0 right-0 z-0 rounded-lg opacity-80;
+    width: var(--process);
+  }
+
+  .title {
+    @apply truncate z-10;
+  }
+
+  .icon {
+    @apply text-slate-500 opacity-50 hover:text-yellow-500 hover:opacity-90
+    hover:scale-125 duration-300 cursor-pointer ml-1;
+  }
+}
+</style>
--- src/renderer/src/components/VideoList.vue
+++ src/renderer/src/components/VideoList.vue
@@ -1,29 +1,19 @@
 <script lang="ts" setup>
-import { CloseOne } from '@icon-park/vue-next'
 import { ref } from 'vue'
+import VideoItem from './VideoItem.vue'
 
-const videos = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+const videos = ref<string[]>([
+  'Video1fadfasdcdasvdasdvasvsavdsavasvasfasfasdvasdvsavsacasdvdsvsacasvdvas',
+  'Video 2',
+  'Video 3',
+  'Video 4'
+])
 </script>
 
 <template>
   <main class="overflow-y-auto h-80">
-    <section v-for="(video, index) in videos" :key="index" class="video">
-      {{ video }}
-      <div class="icon">
-        <CloseOne theme="outline" size="12" />
-      </div>
-    </section>
+    <VideoItem v-for="(video, index) in videos" :key="index" :video="{ title: video }" />
   </main>
 </template>
 
-<style lang="scss" scoped>
-.video {
-  @apply bg-white px-3 py-1 rounded-lg mb-2 mx-3 text-xs text-slate-600 
-  flex justify-between items-center;
-
-  .icon {
-    @apply text-slate-500 opacity-50 hover:text-yellow-500 hover:opacity-90
-    hover:scale-125 duration-300 cursor-pointer;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
