40fa9e11a5aedbfaaf127ec7991b6d4b0288265b
tkzc00 <2238983030@qq.com>
2025-08-17 14:32:23 +0800

    添加进度条动画效果

--- src/renderer/src/components/VideoItem.vue
+++ src/renderer/src/components/VideoItem.vue
@@ -22,13 +22,31 @@ const process = ref(60)
 
 <style lang="scss" scoped>
 .video {
-  @apply bg-white px-3 py-[8px] rounded-lg mb-2 mx-3 text-xs text-slate-600 
+  @apply bg-white px-3 py-[8px] rounded-lg mb-2 mx-3 text-xs text-slate-600
   flex justify-between items-center relative;
 
   &::before {
     content: '';
-    @apply bg-yellow-300 absolute top-0 bottom-0 left-0 right-0 z-0 rounded-lg opacity-80;
+    @apply absolute top-0 bottom-0 left-0 right-0 z-0 rounded-lg opacity-80;
     width: var(--process);
+    background: repeating-linear-gradient(
+      -45deg,
+      #c4ff8e 0px,
+      #c4ff8e 10px,
+      #a8ed6a 10px,
+      #a8ed6a 20px
+    );
+    background-size: 28.28px 28.28px;
+    animation: moveRight 1s linear infinite;
+  }
+
+  @keyframes moveRight {
+    0% {
+      background-position-x: 0;
+    }
+    100% {
+      background-position-x: 28.28px;
+    }
   }
 
   .title {
